eb8ea4e193e254c787b7c98d9deee27ef748c39f
ThomasGuy <twguy66@gmail.com>
2018-07-25 23:56:34 +0100

    fixed for es6

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+	const doc = document.documentElement;
+	const body = doc.body;
+	const _head = doc.firstChild;
+	const _body = doc.lastChild;
+	const _head_ = doc.childNodes[0];
+	const title = _head.firstChild;
+	// alert(_head.parentNode === doc);
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,46 @@
-var gulp        = require('gulp');
-var harp        = require('harp');
-var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
-var prefixer    = require('gulp-autoprefixer');
-var reload      = browserSync.reload;
-var plumber     = require('gulp-plumber');
-var beeper      = require('beeper');
+const gulp = require('gulp');
+const harp = require('harp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const prefixer = require('gulp-autoprefixer');
+
+let reload = browserSync.reload;
+const plumber = require('gulp-plumber');
+const beeper = require('beeper');
 
 // Error Helper
 function onError(err) {
-  beeper();
-  console.log('Name:', err.name);
-  console.log('Reason:', err.reason);
-  console.log('File:', err.file);
-  console.log('Line:', err.line);
-  console.log('Column:', err.column);
-  //this.emit('end');
+	beeper();
+	console.log('Name:', err.name);
+	console.log('Reason:', err.reason);
+	console.log('File:', err.file);
+	console.log('Line:', err.line);
+	console.log('Column:', err.column);
+	// this.emit('end');
 }
 
 // Static Server + watching sass/html files
-gulp.task('serve', ['sass'], function() {
-    browserSync.init({
-      server: './app'
-    });
-    gulp.watch('app/css/**/*.sass', [ 'sass']);
-    gulp.watch('app/*.html').on('change', reload);
+gulp.task('serve', ['sass'], () => {
+	browserSync.init({
+		server: './app',
+	});
+	gulp.watch('app/css/**/*.sass', ['sass']);
+	gulp.watch('app/*.html').on('change', reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
-gulp.task('sass',function() {
-  return gulp.src('app/css/sass/main.sass')
-    .pipe(plumber({
-      errorHandler : onError
-      }))
-    .pipe(sass())
-    .pipe(prefixer({
-              browsers: ['last 2 versions'],
-              cascade: false
-          }))
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.stream());
+gulp.task('sass', () => {
+	return gulp.src('app/css/sass/main.sass')
+		.pipe(plumber({
+			errorHandler: onError,
+		}))
+		.pipe(sass())
+		.pipe(prefixer({
+			browsers: ['last 2 versions'],
+			cascade: false,
+		}))
+		.pipe(gulp.dest('app/css'))
+		.pipe(browserSync.stream());
 });
 
 gulp.task('default', ['serve']);
